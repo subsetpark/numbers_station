1c7d09fec463637e648fb52c80ad19c24c2da19a
Z. D. Smith <cruxdestruct@gmail.com>
2015-01-05 19:39:45 -0500

    remove RESTy call

--- src/numbers_handler.erl
+++ src/numbers_handler.erl
@@ -20,23 +20,12 @@ init(_Route, _Req, State) ->
 terminate(_Reason, _Route, _Req, _State) ->
     ok.
 
-get("/series", _Req, State) ->
-    Query = fun() ->
-            qlc:e(
-                qlc:q([X || X <- mnesia:table(numbers)])
-                )
-    end,
-    {atomic, Records} = mnesia:transaction(Query),
-    Json = numbers_helper:format(Records),
-    {200, {json, Json}, State};
-
 get("/series/:id/:n", Req, State) ->
     Id = binary_to_atom(leptus_req:param(Req, id), unicode),
     N = binary_to_integer(leptus_req:param(Req, n)),
 
     Result = get_term(Id, N),
 
-    io:format('~p', [Result]),
     Json = numbers_helper:format([Result]),
 
     {200, {json, Json}, State}.
