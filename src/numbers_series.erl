2ebe160b1ec726c5c9fe329594c07dc785c7aff5
Z. D. Smith <cruxdestruct@gmail.com>
2015-01-05 22:56:06 -0500

    Lose .rebar

--- src/numbers_series.erl
+++ src/numbers_series.erl
@@ -1,9 +1,11 @@
 -module(numbers_series).
 -export([fibonacci/1, natural/1]).
 
+-spec natural(integer()) -> integer().
 natural(N) ->
     N.
 
+-spec fibonacci(integer()) -> integer().
 fibonacci(1) -> 1;
 fibonacci(2) -> 1;
 fibonacci(N) when N > 2 -> fib(N, 1, 1).
